cf943c0628a3f583191265711c497f8023708565
Vaishnavi Shastrula <VaishnaviShastrula@my.unt.edu>
2024-10-16 15:30:23 -0500

    Update auth.js

--- toy_station/server/routes/auth.js
+++ toy_station/server/routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const { register, login } = require('../controllers/authController');
 const router = express.Router();
 
+// Route for user registration
 router.post('/register', register);
+
+// Route for user login
 router.post('/login', login);
 
 module.exports = router;
