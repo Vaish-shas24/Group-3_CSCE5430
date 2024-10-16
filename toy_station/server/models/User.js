9287dc25f6593270490308111c3b3d93ea2efaa9
Vaishnavi Shastrula <VaishnaviShastrula@my.unt.edu>
2024-10-16 15:33:42 -0500

    Update User.js

--- toy_station/server/models/User.js
+++ toy_station/server/models/User.js
@@ -1,19 +1,25 @@
 const mongoose = require('mongoose');
 
+// Define the schema for the User model
 const UserSchema = new mongoose.Schema({
+  // Username of the user
   username: {
     type: String,
-    required: true,
+    required: true, // This field is mandatory
   },
+  // Email of the user (must be unique)
   email: {
     type: String,
-    required: true,
-    unique: true,
+    required: true, // This field is mandatory
+    unique: true, // Ensures email is unique across all users
   },
+  // Password for the user's account
   password: {
     type: String,
-    required: true,
+    required: true, // This field is mandatory
   },
 });
 
+// Export the User model for use in other parts of the application
 module.exports = mongoose.model('User', UserSchema);
+
