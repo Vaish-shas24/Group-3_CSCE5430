f15d7c306ccef1790ccb8ed4d6725d4671582bd0
TejaNagendraSirigineedi <tejanagendrasirigineedi@my.unt.edu>
2024-11-11 23:49:50 -0600

    Update server.js

--- toy_station/server/server.js
+++ toy_station/server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/orders', orderRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
