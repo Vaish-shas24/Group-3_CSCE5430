386afa9184ccf234804e80192b7f1005dd894659
Gayathri-Vutla <GayathriVutla@my.unt.edu>
2024-12-02 18:49:46 -0600

    Update App.js

--- toy_station/client/src/App.js
+++ toy_station/client/src/App.js
@@ -10,11 +10,20 @@ import store from './redux/store';
 import Login from './components/Login';
 import Register from './components/Register';
 import HelpSupport from './components/HelpSupport';
+import AboutUs from './components/AoutUs';
+import ContactUs from './components/ContactUs';
+import { Elements } from '@stripe/react-stripe-js'; // Import Elements
+import { loadStripe } from '@stripe/stripe-js';
+
+
+const stripePromise = loadStripe('pk_test_51QKcIFFQuaYsVoVkDIa1bzysWr4MZrPB3RZXVhh7fNQjhfnFCwMBMq1mKchkHZcXwKjxak70drBVAoRNAp3zB4EA00RcBqr2aD');
+
 
 function App() {
   return (
     <Provider store={store}>
     <Router>
+    <Elements stripe={stripePromise}>
     <div>
       <Routes>
         <Route path="/home" element={<Home />} />
@@ -24,12 +33,15 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/help-support" element={<HelpSupport />} />
+        <Route path="/AboutUS" element={<AboutUs />} />
+        <Route path="/ContactUs" element={<ContactUs />} />
       </Routes>
     </div>
+    </Elements>
+
   </Router>
   </Provider>
   );
 }
 
 export default App;
-
