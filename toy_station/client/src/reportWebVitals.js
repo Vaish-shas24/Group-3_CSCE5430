cc8f7abff871dec04f570fd187792d6b9c6cc158
Gayathri-Vutla <GayathriVutla@my.unt.edu>
2024-12-02 18:53:39 -0600

    Update reportWebVitals.js

--- toy_station/client/src/reportWebVitals.js
+++ toy_station/client/src/reportWebVitals.js
@@ -1,18 +1,13 @@
-// Function to report web performance metrics (Web Vitals)
-const reportWebVitals = onPerfEntry => {
-  // Check if onPerfEntry is a function before proceeding
+onst reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    // Dynamically import the web-vitals library and extract the required functions
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      // Call each Web Vital function and pass the performance entry callback
-      getCLS(onPerfEntry);  // Cumulative Layout Shift
-      getFID(onPerfEntry);  // First Input Delay
-      getFCP(onPerfEntry);  // First Contentful Paint
-      getLCP(onPerfEntry);  // Largest Contentful Paint
-      getTTFB(onPerfEntry); // Time to First Byte
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
     });
   }
 };
 
 export default reportWebVitals;
-
